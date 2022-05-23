b0fc06f49b87d37aa63f2ade68b8c9af7a23bdfa
NT <usedforfunny@gmail.com>
2022-05-23 17:22:27 +0200

    Initial Commit

---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Alexandre Talatinian on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
